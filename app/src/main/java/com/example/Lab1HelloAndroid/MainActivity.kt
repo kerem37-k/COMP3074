9f192fd35fb01aeae7f257bf13a6c0d08ccb12c1
kerembas <kbas5681@gmail.com>
2026-09-13 14:50:51 -0400

    COMP3074 Lab 1

---
+++ app/src/main/java/com/example/Lab1HelloAndroid/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.Lab1HelloAndroid
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val message = findViewById<TextView>(R.id.textViewMessage)
+        val button = findViewById<Button>(R.id.buttonChangeText)
+
+        button.setOnClickListener {
+            message.text = "Button clicked!"
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
