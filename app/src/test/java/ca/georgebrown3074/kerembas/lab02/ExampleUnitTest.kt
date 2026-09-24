70ac4d3282a486a5395d0d2cc07a787ad5b71b85
kerembas <kbas5681@gmail.com>
2026-09-24 16:09:42 -0400

    COMP3074 Lab 2

---
+++ app/src/test/java/ca/georgebrown3074/kerembas/lab02/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package ca.georgebrown3074.kerembas.lab02
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
